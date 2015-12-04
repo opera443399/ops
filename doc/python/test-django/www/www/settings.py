fc1a210b76ea299f51fd7896eb59d97a0b629721
opera443399 <pc@pcswo.com>
2015-12-04 19:01:39 +0800

    update test-django

--- doc/python/test-django/www/www/settings.py
+++ doc/python/test-django/www/www/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^&i3e_kwe)jhko2xct&l%t*6^mjt=&ckyz16omv6c5hyx(xded'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,3 +103,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
