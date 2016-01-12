3d2a4608ec2593a4fab786cad6904197dc7618c0
opera443399 <pc@pcswo.com>
2016-01-12 19:09:03 +0800

    rebuild, use jquery and bootstrap

--- doc/python/test-django/www/static/charade/js/main.js
+++ doc/python/test-django/www/static/charade/js/main.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
     $("#quote").hide();
     $("#help").hide();
-    $("#tips").click(function(){
+    $("#amount").focus();
+    $("#tips1").click(function(){
         $("#quote").slideToggle("slow");
     });
-    $("#tips").hover(function(){
+    $("#tips2").click(function(){
         $("#help").fadeToggle("slow");
+    });
+
+    $("#setting").submit(function(){
+        var n = $("#amount").val()
+        var r = /^[0-9]*$/;
+        if(!r.test(n)){
+            event.preventDefault();
+            alert("Digit only. '" + n + "' is not valid.");
+        }
     });
 });
