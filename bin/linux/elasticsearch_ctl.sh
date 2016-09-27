10eaf5cc9d8c470c8951477a559b441f36b4265b
opera443399 <pc@pcswo.com>
2016-09-27 17:03:44 +0800

    added: index clean

--- bin/linux/elasticsearch_ctl.sh
+++ bin/linux/elasticsearch_ctl.sh
@@ -1,44 +1,54 @@
 #!/bin/bash
 #
-#2016/9/23
+#2016/9/27
+#ver:0.1.0
+
+dt_default=$(date -d"7 days ago" +"%Y.%m.%d")
 
 show_h(){
     echo -e '\033[32m[+] show health\033[0m'
     echo -e '\033[32m-----------------------------------------------------------\033[0m'
-    curl 'localhost:9200/_cat/health?v'
+    curl -s 'localhost:9200/_cat/health?v'
 }
 
 show_n(){
     echo -e '\033[32m[+] show nodes\033[0m'
     echo -e '\033[32m-----------------------------------------------------------\033[0m'
-    curl 'localhost:9200/_cat/nodes?v'
+    curl -s 'localhost:9200/_cat/nodes?v'
 }
 
 show_i(){
     echo -e '\033[32m[+] show indices\033[0m'
     echo -e '\033[32m-----------------------------------------------------------\033[0m'
-    curl 'localhost:9200/_cat/indices?v'
+    curl -s 'localhost:9200/_cat/indices?v'
 }
 
 index_del(){
     #curl -XDELETE 'http://localhost:9200/filebeat-*?pretty'
-    echo -e "\033[32m[+] delete index: ${1}\033[0m"
+    echo -e "\033[32m[+] delete index: ${1}*\033[0m"
     echo -e '\033[32m-----------------------------------------------------------\033[0m'
-    curl -XDELETE "http://localhost:9200/${1}*?pretty"
+    curl -s -XDELETE "http://localhost:9200/${1}*?pretty"
+}
+
+index_cls(){
+    #curl -XDELETE 'http://localhost:9200/filebeat-*-2016.09.23?pretty'
+    echo -e "\033[32m[+] delete index:  ${1}-*-${2}\033[0m"
+    echo -e '\033[32m-----------------------------------------------------------\033[0m'
+    curl -s -XDELETE "http://localhost:9200/${1}-*-${2}?pretty"
 }
 
 show_t(){
     #curl 'http://localhost:9200/_template/filebeat?pretty'
     echo -e "\033[32m[+] show template for index: ${1}\033[0m"
     echo -e '\033[32m-----------------------------------------------------------\033[0m'
-    curl 'http://localhost:9200/_template/${1}?pretty'
+    curl -s 'http://localhost:9200/_template/${1}?pretty'
 }
 
 tpl_set(){
     #curl -XPUT 'http://localhost:9200/_template/filebeat?pretty' -d@/tmp/elasticsearch.template.filebeat.json
     echo -e "\033[32m[+] set template for index: ${1}\033[0m"
     echo -e '\033[32m-----------------------------------------------------------\033[0m'
-    curl -XPUT 'http://localhost:9200/_template/${1}?pretty' -d@${2}
+    curl -s -XPUT 'http://localhost:9200/_template/${1}?pretty' -d@${2}
 }
 
 
@@ -52,6 +62,7 @@ Usage:  $0 [h|n|ishow|idel|tshow|tset]
     $0 n                                        : list es nodes
     $0 i                                        : list es indices
     $0 idel index-name                          : del index as given
+    $0 icls index-name yyyy.mm.dd               : del index before the day as given
     $0 t template-name                          : show template as given
     $0 tset template-name template-file-path    : set user-define template
 
@@ -66,6 +77,10 @@ case $1 in
         ;;
     idel)
         index_del $2
+        ;;
+    icls)
+        [ -z $3 ] || dt_default=$3
+        index_cls $2 ${dt_default}
         ;;
     tset)
         tpl_set $2 $3
