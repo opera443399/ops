9633607452ba340b3960e132d56683e54767d57a
opera443399 <pc@pcswo.com>
2016-12-09 15:41:25 +0800

    reformatted

--- bin/linux/docker_direct_lvm_setup.sh
+++ bin/linux/docker_direct_lvm_setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #
 #2016/12/8
-#v1.0.4 @PC
+#v1.0.5 @PC
 
+## 分配一块独立的磁盘供docker使用，本例使用的是 /dev/vdb
 dev_name='/dev/vdb'
+
 yum -y -q install lvm2
 echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
 pvcreate ${dev_name}
@@ -24,37 +26,33 @@ echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
 
 cat <<'_EOF'
 
-
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-howto:
+接下来的操作步骤示例：
 
-[1] backup if needed
+[1] 如果 docker 有旧的数据，先推送到registry中再移除；
 # mkdir /var/lib/docker.bk
 # mv /var/lib/docker/* /var/lib/docker.bk
 
-[2] update docker config
-(added to docker.service)
+[2] 更新docker服务的配置：
+(方式一： 调整 docker.service 的配置参数)
 # sed -i '/^ExecStart=/c\ExecStart=/usr/bin/dockerd --storage-driver=devicemapper --storage-opt=dm.thinpooldev=/dev/mapper/docker-thinpool --storage-opt=dm.use_deferred_removal=true --storage-opt=dm.use_deferred_deletion=true' /lib/systemd/system/docker.service 
 
-(added to daemon.json)
+(方式二： 在 daemon.json 中配置参数)
 # cat /etc/docker/daemon.json
 {
-  "storage-driver": "devicemapper",
+    "storage-driver": "devicemapper",
     "storage-opts": [
-      "dm.thinpooldev=/dev/mapper/docker-thinpool",
-      "dm.use_deferred_removal=true",
-      "dm.use_deferred_deletion=true"
+        "dm.thinpooldev=/dev/mapper/docker-thinpool",
+        "dm.use_deferred_removal=true",
+        "dm.use_deferred_deletion=true"
     ]
 }
 
 
-[3] restart
-# systemctl daemon-reload
-# systemctl start docker
+[3] 重启服务
+# systemctl daemon-reload && systemctl start docker
 
-[4] validate
+[4] 验证是否调整完毕
 # docker info
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
 _EOF
