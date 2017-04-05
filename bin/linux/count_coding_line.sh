a99fd13dcb3f75a92af3da6d7a1ba059e77f252c
opera443399 <pc@pcswo.com>
2017-04-05 18:46:03 +0800

    variable dest_dir added.

--- bin/linux/count_coding_line.sh
+++ bin/linux/count_coding_line.sh
@@ -4,8 +4,9 @@
 # pc
 # a simple example for howto count your coding lines 
 
-cd $1
-echo -e "\n\033[1;34m--Count your coding lines from dir: $1--\033[0m"
+dest_dir=$1
+cd ${dest_dir}
+echo -e "\n\033[1;34m--Count your coding lines from dir: ${dest_dir}--\033[0m"
 
 echo -en '\n\033[1;33m-python: \033[0m'
 r1=`find . -name "*.py" |xargs -i grep -v "^$" {} |wc -l`
@@ -16,23 +17,23 @@ r2=`find . -name "*.html" |xargs -i grep -v "^$" {} |wc -l`
 echo $r2
 
 echo -en '\n\033[1;33m-js: \033[0m'
-r3=`find /opt/src/asset/ -name "*.js" |grep -vE "bootstrap|npm|jquery|admin|rest_framework" |xargs -i grep -v "^$" {} |wc -l`
+r3=`find . -name "*.js" |grep -vE "bootstrap|npm|jquery|admin|rest_framework" |xargs -i grep -v "^$" {} |wc -l`
 echo $r3
 
 echo -en '\n\033[1;33m-css: \033[0m'
-r4=`find /opt/src/asset/ -name "*.css" |grep -vE "bootstrap|jquery|admin|rest_framework" |xargs -i grep -v "^$" {} |wc -l`
+r4=`find . -name "*.css" |grep -vE "bootstrap|jquery|admin|rest_framework" |xargs -i grep -v "^$" {} |wc -l`
 echo $r4
 
 echo -en '\n\033[1;33m-json: \033[0m'
-r5=`find /opt/src/asset/ -name "*.json" |grep -vE "bootstrap|jquery|admin|rest_framework" |xargs -i grep -v "^$" {} |wc -l`
+r5=`find . -name "*.json" |grep -vE "bootstrap|jquery|admin|rest_framework" |xargs -i grep -v "^$" {} |wc -l`
 echo $r5
 
 echo -en '\n\033[1;33m-csv: \033[0m'
-r6=`find /opt/src/asset/ -name "*.csv" |grep -vE "bootstrap|jquery|admin|rest_framework" |xargs -i grep -v "^$" {} |wc -l`
+r6=`find . -name "*.csv" |grep -vE "bootstrap|jquery|admin|rest_framework" |xargs -i grep -v "^$" {} |wc -l`
 echo $r6
 
 echo -en '\n\033[1;33m-i18n: \033[0m'
-r7=`find /opt/src/asset/ -name "*.po" |grep -vE "bootstrap|jquery|admin|rest_framework" |xargs -i grep -v "^$" {} |wc -l`
+r7=`find . -name "*.po" |grep -vE "bootstrap|jquery|admin|rest_framework" |xargs -i grep -v "^$" {} |wc -l`
 echo $r7
 
 
