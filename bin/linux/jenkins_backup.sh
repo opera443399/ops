1c9ee2bf9cd54d53b71d258e73f42f889f681e6b
opera443399 <pc@pcswo.com>
2017-08-21 15:05:16 +0800

    chore(jenkins): backup exp

---
+++ bin/linux/jenkins_backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#20170821
+#PC
+#0 13,23 * * * /bin/bash -xe /ops/bin/jenkins_backup.sh >/tmp/test.log 2>&1 &
+
+
+prefix="jenkins_$(hostname)_$(/usr/sbin/ip a |grep global |grep brd |awk '{print $2}' |awk -F'/' '{print $1}')"
+jenkins_home='/var/lib/jenkins'
+jenkins_backup_root='/data/backup/jenkins_data'
+jenkins_backup_file=${jenkins_backup_root}/${prefix}_$(date +%Y%m%d_%H%M%S).tar.gz
+rotate=3
+
+[ -z ${jenkins_backup_root} ] && exit 1
+find ${jenkins_backup_root} -maxdepth 1 -type f -name '*.gz' -mtime +${rotate} -print
+find ${jenkins_backup_root} -maxdepth 1 -type f -name '*.gz' -mtime +${rotate} -delete
+
+tar zcf ${jenkins_backup_file} ${jenkins_home}
