c4550c9c9d4eb2e50c27ee1cc925b3cb67b2d01a
opera443399 <pc@pcswo.com>
2017-09-04 15:00:11 +0800

    chore(jenkins): backup opts changed

--- bin/linux/jenkins_backup.sh
+++ bin/linux/jenkins_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#20170821
+#20170904
 #PC
 #0 13,23 * * * /bin/bash -xe /ops/bin/jenkins_backup.sh >/tmp/test.log 2>&1 &
 
@@ -8,10 +8,11 @@ prefix="jenkins_$(hostname)_$(/usr/sbin/ip a |grep global |grep brd |awk '{print
 jenkins_home='/var/lib/jenkins'
 jenkins_backup_root='/data/backup/jenkins_data'
 jenkins_backup_file=${jenkins_backup_root}/${prefix}_$(date +%Y%m%d_%H%M%S).tar.gz
-rotate=3
+rotate=1
+
 
 [ -z ${jenkins_backup_root} ] && exit 1
 find ${jenkins_backup_root} -maxdepth 1 -type f -name '*.gz' -mtime +${rotate} -print
 find ${jenkins_backup_root} -maxdepth 1 -type f -name '*.gz' -mtime +${rotate} -delete
 
-tar zcf ${jenkins_backup_file} ${jenkins_home}
+tar zcf ${jenkins_backup_file} ${jenkins_home} --exclude "${jenkins_home}/workspace"
