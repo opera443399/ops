316ee3be8e89afff7d100c8b76ab060b31bdcc6a
opera443399 <pc@pcswo.com>
2017-10-31 17:20:51 +0800

    chore(go): refactor the example

--- doc/Go/src/abc.com/demo/demoHttpHead/app.go
+++ doc/Go/src/abc.com/demo/demoHttpHead/app.go
@@ -1,25 +1,42 @@
 /*
 # demoHttpHead
-# 2017/10/20
-*/ 
+# 2017/10/31
+*/
 
 package main
 
 import (
     "flag"
     "fmt"
+    "io/ioutil"
     "log"
+    "net/http"
+    "runtime"
+    "strconv"
+    "strings"
     "sync"
     "time"
-    "net/http"
-    "io/ioutil"
-    "strings"
-    "strconv"
 )
 
+const defaultTestURL1 = "http://www.baidu.com"
+const defaultTestURL2 = "http://www.taobao.com"
+const defaultCfg = "urls.txt"
+const defaultFrequency = 10
+const defaultTimeout = 1
+const defaultStdout = false
+
+var cnt int
+var fromCfg bool
+var targetWebsites []string
+
+func init() {
+    flag.IntVar(&cnt, "c", defaultFrequency, "[] repeat N times to request the URL.")
+    flag.BoolVar(&fromCfg, "f", false, "[] load URLs from file. (default: "+defaultCfg+")")
+    runtime.GOMAXPROCS(runtime.NumCPU())
+}
 
 type taskState struct {
-    v map[string]int
+    v   map[string]int
     mux sync.Mutex
 }
 
@@ -35,73 +52,83 @@ func (ts *taskState) Value(key string) int {
     return ts.v[key]
 }
 
-
-func handleError(err error, method string) bool {
+func loadDataFromFile() []string {
+    var urls []string
+    data, err := ioutil.ReadFile(defaultCfg)
     if err != nil {
-        log.Printf("[E] %s : %v", method, err)
-        return true
-    }
-    return false
-}
-
-
-func getHosts() ([]string) {
-    var hosts []string
-    data, err := ioutil.ReadFile("urls.txt")
-    if handleError(err, "ioutil.ReadFile") {
-        return hosts
+        log.Printf("[E] %s : %v", "ioutil.ReadFile", err)
+        return urls
     }
 
     for _, h := range strings.Split(string(data), "\n") {
         if h == "" {
             continue
         }
-        hosts = append(hosts, h)
+        urls = append(urls, h)
     }
-    return hosts
+    return urls
 }
 
-
-func handleHttpHeadReq(seq int, url string, ch chan string, stat *taskState) {
-    head, err := http.Head(url)
-    if handleError(err, "http.Head") {
-        stat.Inc("failure")
-
-        ch <- "[" + strconv.Itoa(seq)  + "]" + url + " : failed."
-        return
+func handleHTTPHeadRequest(cnt int, url string, ch chan string, stat *taskState) {
+    for i := 0; i < cnt; i++ {
+        header, err := http.Head(url)
+        if err != nil {
+            log.Printf("[E] %s : %v", "http.Head", err)
+            stat.Inc("failure")
+            ch <- "[" + strconv.Itoa(i) + "]" + url + " : failed."
+        } else {
+            stat.Inc("success")
+            status := header.Status
+            ch <- "[" + strconv.Itoa(i) + "]" + url + " : " + status
+        }
     }
 
-    stat.Inc("success")
-    status := head.Status
-    ch <- "[" + strconv.Itoa(seq) +  "]" + url + " : " + status
 }
-
 
 func main() {
-    dtStart := time.Now()
-    cnt := flag.Int("c", 10, "[] set N times to request the http url.")
-    fromCfg := flag.Bool("f", false, "[] parse urls from file: [urls.txt] or not?")
-
     flag.Parse()
-
-    var stat = taskState{v: make(map[string]int)}
-    var hosts []string
-
-    if *fromCfg == true {
-        hosts = getHosts()
-    } else{
-        hosts = flag.Args()
+    if fromCfg == true {
+        targetWebsites = loadDataFromFile()
+    } else if len(flag.Args()) > 0 {
+        targetWebsites = flag.Args()
+    } else {
+        targetWebsites = []string{defaultTestURL1, defaultTestURL2}
     }
 
-    for _, url := range hosts {
-        ch := make(chan string)
-        for i := 0; i < *cnt; i++ {
-            go handleHttpHeadReq(i, url, ch, &stat)
-        }
+    dtStart := time.Now()
+    stat := taskState{v: make(map[string]int)}
+    ch := make(chan string)
+    chTheEnd := make(chan bool)
 
-        for t := 0; t < *cnt; t++ {
-            fmt.Println(<-ch)
-        }
+    for _, url := range targetWebsites {
+        go handleHTTPHeadRequest(cnt, url, ch, &stat)
     }
+
+    go func() {
+        timer := time.NewTimer(time.Second * defaultTimeout)
+        for {
+            if !timer.Stop() {
+                select {
+                case <-timer.C: //try to drain from the channel
+                default:
+                }
+            }
+            timer.Reset(time.Second * defaultTimeout)
+            select {
+            case msg, ok := <-ch:
+                if ok {
+                    fmt.Println(msg)
+                    continue
+                }
+                chTheEnd <- true
+            case <-timer.C:
+                log.Printf("timer expired (%ds)", defaultTimeout)
+                chTheEnd <- true
+            }
+        }
+    }()
+
+    <-chTheEnd
     log.Printf("success: %d, failure: %d, Time Cost: %v\n", stat.Value("success"), stat.Value("failure"), time.Since(dtStart))
+
 }
