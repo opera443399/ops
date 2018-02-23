345397b1a21345c384e56072d6c0eaf77875db88
opera443399 <pc@pcswo.com>
2018-02-23 17:56:30 +0800

    fix(k8s-setup-ha): add sysctl

--- doc/CLOUD/k8s/k8s集群部署手册/init_offline/init-hosts.sh
+++ doc/CLOUD/k8s/k8s集群部署手册/init_offline/init-hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# 2018/2/7
+# 2018/2/23
 set -e
 
 tee /etc/hosts <<-'_EOF'
@@ -17,3 +17,11 @@ tee /etc/hosts <<-'_EOF'
 10.222.0.200 worker-200
 
 _EOF
+
+
+cat <<'_EOF' >/etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+_EOF
+
+sysctl --system
