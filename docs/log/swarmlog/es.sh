c0c91105e5a063786c28217e2f1181a66cadcced
opera443399 <pc@pcswo.com>
2018-07-26 13:24:10 +0800

    fix(swarmlog): arguments missed

--- docs/log/swarmlog/es.sh
+++ docs/log/swarmlog/es.sh
@@ -2,19 +2,23 @@
 #
 # 2018/7/25
 
+sysctl -w vm.max_map_count=262144
+
 test $(docker ps -a -f name=logs-elasticsearch -q |wc -l) -eq 0 || \
 docker rm -f $(docker ps -a -f name=logs-elasticsearch -q)
 
 # ports: 9200[elasticsearch]
 docker run -d -p "9200:9200" \
+    --ulimit memlock=-1:-1 \
     --name logs-elasticsearch \
     -v /etc/localtime:/etc/localtime \
     -v /etc/timezone:/etc/timezone \
     -v /data/server/swarmlog/es:/usr/share/elasticsearch/data \
+    -e "cluster.name=docker-logs-cluster" \
+    -e "bootstrap.memory_lock=true" \
+    -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
     -e "http.host=0.0.0.0" \
-    -e "transport.host=localhost" \
+    -e "transport.host=0.0.0.0" \
     -e "network.host=0.0.0.0" \
     -e "xpack.security.enabled=false" \
-    -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
-    -e "bootstrap.memory_lock=true" \
     elasticsearch:5.6.10
--- docs/log/swarmlog/graylog.sh
+++ docs/log/swarmlog/graylog.sh
@@ -19,4 +19,6 @@ docker run -d -p "9000:9000" -p "514:514" -p "514:514/udp" -p "12201:12201" -p "
     -e "GRAYLOG_ROOT_PASSWORD_SHA2=8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918" \
     -e "GRAYLOG_WEB_ENDPOINT_URI=http://10.50.200.101:9000/api" \
     -e "GRAYLOG_MONGODB_URI=mongodb://10.50.200.101/graylog" \
+    -e "GRAYLOG_ELASTICSEARCH_HOSTS=http://10.50.200.101:9200" \
+    -e "GRAYLOG_ROOT_TIMEZONE=Asia/Shanghai" \
     graylog/graylog:2.4.6-1
