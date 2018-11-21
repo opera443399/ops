999b9f0cd47dd017b243f32e28ada8a566e2eda0
opera443399 <pc@pcswo.com>
2018-11-21 15:45:16 +0800

    chore(jenkins): improve backup script

---
+++ CICD/jenkins/bin/backup_jenkins.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# 2018/11/21
+#0 2 * * * /bin/bash -xe /usr/local/bin/backup_jenkins.sh >>/data/logs/backup/jenkins.log 2>&1 &
+
+this_ip="$(/usr/sbin/ip a |grep global |grep brd |grep 'eth0' |awk '{print $2}' |awk -F'/' '{print $1}')"
+jenkins_home='/data/server/jenkins/data'
+jenkins_backup_root="/data/backup/jenkins/${this_ip}"
+[ -d ${jenkins_backup_root} ] || mkdir -p ${jenkins_backup_root}
+jenkins_backup_file=${jenkins_backup_root}/${this_ip}_$(date +%Y%m%d_%H%M%S).tar.gz
+rotate=7
+
+
+[ -z ${jenkins_backup_root} ] && exit 1
+find ${jenkins_backup_root} -maxdepth 1 -type f -name '*.gz' -mtime +${rotate} -print
+find ${jenkins_backup_root} -maxdepth 1 -type f -name '*.gz' -mtime +${rotate} -delete
+
+tar zcf ${jenkins_backup_file} ${jenkins_home}
+
+# remote backup
+rsync -avzP /data/backup/jenkins/ 10.50.200.101:/data/backup/jenkins/
