10a44e74b4a566a1d3979b61b6a4548f6fba17cc
opera443399 <pc@pcswo.com>
2019-01-19 09:59:07 +0800

    chore(dockerfile): enhance demo

--- CICD/jenkins/sample/pipeline/swarm/ci/init.sh
+++ CICD/jenkins/sample/pipeline/swarm/ci/init.sh
@@ -6,15 +6,22 @@ grep "^[^#]" svc.list |awk -F',' '{print $1" "$2}' >.svc
 
 while read svc_name svc_port
 do
-  [ -d "tpl.docker.d/${svc_name}" ] && continue
+  [ -f "tpl.docker.d/${svc_name}/Dockerfile" ] && continue
   echo "[+] CI init ${svc_name} ${svc_port}"
   mkdir -pv "tpl.docker.d/${svc_name}"
   cat <<_EOF > "tpl.docker.d/${svc_name}/Dockerfile"
 FROM ns-demo/image-base-demo:v0.1
 
-COPY ${svc_name} .
+ENV D_APP_BIN '/data/server/${app_name}/'
+
+COPY ${svc_name} \${D_APP_BIN}
+
+RUN mkdir -p \${D_APP_BIN}/logs
+WORKDIR \${D_APP_BIN}
+
 EXPOSE ${svc_port}
 ENTRYPOINT ["./${svc_name}"]
+
 _EOF
 
 done < .svc
